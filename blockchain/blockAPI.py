095e1fe65a82369d807a83186c437c36b8285c5c
DDongu <ehdrb1205@naver.com>
2024-10-07 20:30:44 +0900

    ..

--- blockchain/blockAPI.py
+++ blockchain/blockAPI.py
@@ -55,19 +55,6 @@ blockchain.add_node(node_id)  # 본인 IP를 노드에 추가
 # ==========================================================================================
 
 
-class LogFileHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.src_path == config.LOGFILE:  # 변경된 파일이 로그 파일일 때
-            with open(config.LOGFILE, 'rb') as f:
-                files = {'file': f}
-                response = requests.post(
-                    'http://192.168.0.68/upload', files=files)
-                if response.status_code == 200:
-                    print("로그 파일이 성공적으로 전송되었습니다.")
-                else:
-                    print("로그 파일 전송 실패:", response.text)
-
-
 def reset_consensus_state():
     global consensus_nums, log, consensus_done, get_pre_msg, get_commit_msg
     consensus_nums = 0
