324f429857f875bdb67a475faa1a93c6e71a8870
DDongu <ehdrb1205@naver.com>
2024-10-10 19:10:51 +0900

    Modify diffrent Asynchronous log transmission processing

--- blockchain/log_file_handler.py
+++ blockchain/log_file_handler.py
@@ -53,40 +53,43 @@
 
 #     return logger
 
+
 import logging
-import aiohttp
-import asyncio
+import requests
+import threading
+import queue
 
 class LogServerHandler(logging.Handler):
     def __init__(self, server_url, log_file_name):
         super().__init__()
         self.server_url = server_url
         self.log_file_name = log_file_name
-
-    async def send_log(self, log_entry):
-        async with aiohttp.ClientSession() as session:
-            try:
-                async with session.post(self.server_url, json={
-                    'log': log_entry,
-                    'file_name': self.log_file_name
-                }) as response:
-                    if response.status != 200:
-                        print(f"Failed to send log entry. Status code: {response.status}")
-            except Exception as e:
-                print(f"Error while sending log entry: {e}")
+        self.log_queue = queue.Queue()
+        self.thread = threading.Thread(target=self.process_logs)
+        self.thread.start()
 
     def emit(self, record):
         log_entry = self.format(record)
+        self.log_queue.put(log_entry)
 
-        # 현재 이벤트 루프를 가져오고, 없으면 새로 생성
-        try:
-            loop = asyncio.get_running_loop()
-        except RuntimeError:  # 현재 루프가 없으면
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
+    def process_logs(self):
+        while True:
+            log_entry = self.log_queue.get()
+            if log_entry is None:  # None을 받으면 종료
+                break
+            try:
+                response = requests.post(self.server_url, json={
+                    'log': log_entry,
+                    'file_name': self.log_file_name
+                })
+                if response.status_code != 200:
+                    print(f"Failed to send log entry. Status code: {response.status_code}")
+            except Exception as e:
+                print(f"Error while sending log entry: {e}")
 
-        # 비동기 작업으로 로그 전송
-        loop.create_task(self.send_log(log_entry))
+    def stop(self):
+        self.log_queue.put(None)  # 스레드 종료를 위해 None 추가
+        self.thread.join()  # 스레드가 종료될 때까지 대기
 
 def setup_logging(log_file_path, server_url):
     logging.basicConfig(filename=log_file_path, filemode='w', level=logging.INFO)
